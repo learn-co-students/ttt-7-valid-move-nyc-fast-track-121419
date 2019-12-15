1861194dbf11a4b5bad616d0fafb73cd7d0a1599
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2019-12-15 16:12:15 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
-# code your #valid_move? method here
+def position_taken
+  if position_taken?
+
+end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
